84c68f96f95e42f85ff4202a5eb80aa4c34f44f0
Sidewinder1138 <andersonlambda@gmail.com>
2022-08-31 11:48:02 -0700

    Fetching / on staging neutron OK

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,11 +1,27 @@
 'use strict';
+const axios = require('axios');
+
+const NeutronURL = 'https://neutron-staging.opendock.com';
+
 console.log('---------------------------------------------------');
 console.log("Hello World, I'm a simple Opendock REST API Example");
 console.log('---------------------------------------------------');
+console.log(`   - Base url: ${NeutronURL}`);
 console.log();
 
 async function main() {
+  const api = axios.create({
+    baseURL: NeutronURL,
+  });
+  
 
+  // Let's just see if the server is running:
+  const res = await api.get('/');
+  console.log('* Neutron says:\n' + res.data);
+  console.log();
+
+  // Now let's try and login using the supplied email/password:
+  
   
 }
-await main();
+main();
