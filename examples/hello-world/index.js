3ea0e077166feaa3e5921ac934a441c2c8491e3d
Sidewinder1138 <andersonlambda@gmail.com>
2022-08-31 11:38:01 -0700

    Can run "npm run hello" to run the example

---
+++ examples/hello-world/index.js
@@ -0,0 +1,11 @@
+'use strict';
+console.log('---------------------------------------------------');
+console.log("Hello World, I'm a simple Opendock REST API Example");
+console.log('---------------------------------------------------');
+console.log();
+
+async function main() {
+
+  
+}
+await main();
