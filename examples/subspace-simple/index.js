f4022dcaff207f878edb8580b42bed575dfa83f7
Sidewinder1138 <andersonlambda@gmail.com>
2022-09-06 16:20:32 -0700

    started attempting to listen over subspace, right now the socket is never making it out of "opening" state (not really connecting)

--- examples/subspace-simple/index.js
+++ examples/subspace-simple/index.js
@@ -1,7 +1,9 @@
 'use strict';
 const axios = require('axios');
+const io = require('socket.io-client');
 
 const NeutronURL = 'https://neutron-staging.opendock.com';
+const SubspaceURL = 'wss://subspace.opendock.com/socket.io/';
 
 async function main() {
   const args = process.argv.slice(2);
@@ -41,7 +43,44 @@ async function main() {
   console.log(`* Fetched "me" OK: Hello ${user.firstName} ${user.lastName}! Your access role = "${user.role}"`);
   console.log();
 
-  
+  // We can connect to subspace now, we simply set the 'token' URL parameter to the access token
+  // that we just obtained:
+  console.log(`* Connecting to subspace: ${SubspaceURL}`);
+  const url = `${SubspaceURL}?token=${accessToken}&EIO=3&transport=websocket`;
+  console.log('url=', url);
+
+  // It's important to set the desired transport mechanism to 'websocket':
+  const socket = io(url, { transports: ['websocket'] });  
+  console.log(io.protocol);
+
+  socket.on('connect_error', (error) => {
+    console.log('connect_error!', error);
+  });
+
+  socket.on('connect', () => {
+    console.log('connect=', socket.connected);
+  })
+
+  socket.io.on('error', (error) => {
+    console.log('Error!', error);
+  });
+
+  socket.on('ping', () => {
+    console.log('ping!');
+  });
+
+  socket.on('heartbeat', (thing) => {
+    console.log('heartbeat=', thing);
+  });
+
+  socket.on('update-Appointment', (data) => {
+    console.log('appt update:', data);
+  });
+
+  while (true) {
+    console.log(socket);
+  }
+
 
 }
 main();
