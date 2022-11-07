d0fa4a9218d529ed3bfa6fdee0a7e1dce22c16b4
Sidewinder1138 <andersonlambda@gmail.com>
2022-11-07 14:57:56 -0700

    got hello world express server working

--- examples/refnum-validator/index.js
+++ examples/refnum-validator/index.js
@@ -1,4 +1,19 @@
+import express from "express";
+
+const AppTitle = "RefNum Validator Example";
+const PORT = 8080;
+
 async function main() {
-  console.log("RefNum Validator Middleware Example");
+  console.log(AppTitle);
+
+  const app = express();
+
+  app.get("/", (req, res) => {
+    res.send(`${AppTitle}: Hello World!`);
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+  });
 }
 main();
