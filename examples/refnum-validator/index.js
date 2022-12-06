2f2b0fb5c16331f99661001ece1adabab4bc3c3e
Sidewinder1138 <andersonlambda@gmail.com>
2022-12-06 14:02:54 -0700

    Updated to allow testing of new passcode system, and to work with validation that runs even with no refnum change.

--- examples/refnum-validator/index.js
+++ examples/refnum-validator/index.js
@@ -15,7 +15,7 @@ async function main() {
   console.log(AppTitle);
 
   if (process.argv.length === 3) {
-    PORT = Number(process.argv[2])
+    PORT = Number(process.argv[2]);
   }
 
   const app = express();
@@ -42,43 +42,77 @@ async function main() {
     // is already a JSON object at this point:
     const ctx = req.body;
     if (debug) {
-      console.log('ctx=', JSON.stringify(ctx, null, 2));
+      console.log("ctx=", JSON.stringify(ctx, null, 2));
     }
-    
-    // We can always read the Reference Number from the appointmentFields object:
-    const refNumber = ctx.appointmentFields.refNumber;
-    console.log("* Got Ref Number:", refNumber);
 
-    // Here we enforce a rule that ensures all refnums start with the characters 'ABC':
-    if (!refNumber.startsWith("ABC")) {
-      console.log('* RefNum Bad: must start with "ABC"');
-      // We reply with an HTTP error code, and also send back a JSON object containing a custom
-      // error message, which will be displayed in the Opendock UI to our user, to help them
-      // correct the mistake:
-      return res.status(500).json({
-        errorMessage: "Reference Number must start with 'ABC'",
-      });
+    // If this is a "create", then we can try to read the Reference Number from the
+    // appointmentFields object. If this is an "update", then we can first look to
+    // appointmentFields (in case user is changing the refnum), but if it isn't there,
+    // then we can look in the existingAppointment object:
+    let refNumber = ctx.appointmentFields.refNumber;
+    if (ctx.action === "update" && !refNumber) {
+      refNumber = ctx.existingAppointment.refNumber;
+    }
+
+    if (refNumber) {
+      console.log("* Got Ref Number:", refNumber);
+
+      // Enforce the "passcode", but only if the RefNum contains the text ":PASS:".
+      // Normally, in a *REAL* validator, you would always want to enforce the passcode, but
+      // this way for demo purposes, we can turn enforcement on/off:
+      if (refNumber.includes(":CODE:")) {
+        // Read the passcode out of auth header:
+        const passcode = req.headers.authorization;
+
+        if (!passcode) {
+          return res.status(500).json({
+            errorMessage: "Passcode not set!",
+          });
+        }
+
+        const expectedCode = "123456";
+        if (passcode !== `Bearer ${expectedCode}`) {
+          return res.status(500).json({
+            errorMessage: "Passcode incorrect!",
+          });
+        }
+      }
+
+      // Here we enforce a rule that ensures all refnums start with the characters 'ABC':
+      if (!refNumber.startsWith("ABC")) {
+        console.log('* RefNum Bad: must start with "ABC"');
+        // We reply with an HTTP error code, and also send back a JSON object containing a custom
+        // error message, which will be displayed in the Opendock UI to our user, to help them
+        // correct the mistake:
+        return res.status(500).json({
+          errorMessage: "Reference Number must start with 'ABC'",
+        });
+      }
     }
 
     // Play with the start date/time:
-    const startStr = ctx.action === 'create' ? ctx.appointmentFields.start : ctx.existingAppointment.start;
-    const start = DateTime.fromISO(startStr).setZone('America/Phoenix');
-    console.log('start=', start.toLocaleString(DateTime.DATETIME_FULL));
+    const startStr =
+      ctx.appointmentFields.start || ctx.existingAppointment?.start;
+    const start = DateTime.fromISO(startStr).setZone("America/Phoenix");
+    console.log("start=", start.toLocaleString(DateTime.DATETIME_FULL));
     // Don't allow appointments that start at 10:00 AM (America/Phoenix):
     if (start.hour == 10 && start.minute == 0) {
       return res.status(500).json({
-        errorMessage: "Appointments at 10:00 AM are not allowed, choose another time."
-      })
+        errorMessage:
+          "Appointments at 10:00 AM are not allowed, choose another time.",
+      });
     }
 
     // Play with custom fields:
-    const customFields = ctx.appointmentFields.customFields || ctx.existingAppointment?.customFields;
-    console.log('customFields=', customFields);
+    const customFields =
+      ctx.appointmentFields.customFields ||
+      ctx.existingAppointment?.customFields;
+    console.log("customFields=", customFields);
     // Grab out pallet count:
     let palletCount = null;
     for (const field of customFields) {
-      if (field.label === 'Pallet Count') {
-        palletCount = Number(field.value)
+      if (field.label === "Pallet Count") {
+        palletCount = Number(field.value);
         break;
       }
     }
@@ -86,13 +120,13 @@ async function main() {
     if (palletCount !== null) {
       if (palletCount > 26) {
         return res.status(500).json({
-          errorMessage: `Pallet count of ${palletCount} is too high (max 26).`
-        })
+          errorMessage: `Pallet count of ${palletCount} is too high (max 26).`,
+        });
       }
       if (palletCount < 5) {
         return res.status(500).json({
-          errorMessage: `Pallet count of ${palletCount} is too low (min 5).`
-        })
+          errorMessage: `Pallet count of ${palletCount} is too low (min 5).`,
+        });
       }
     }
 
